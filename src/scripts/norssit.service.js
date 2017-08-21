5aa298b91da3e401d369f67946281e05afe8cb1f
Petri Leskinen <petri.leskinen@aalto.fi>
2017-08-21 16:51:30 +0300

    Update, converting to NBF-data

--- src/scripts/norssit.service.js
+++ src/scripts/norssit.service.js
@@ -37,7 +37,7 @@
             // Text search facet for name
             entryText: {
                 facetId: 'entryText',
-                predicate: '<http://ldf.fi/schema/person_registry/entryText>',
+                predicate: '<http://ldf.fi/nbf/biography>/<http://schema.org/description>',
                 name: 'Haku',
                 enabled: true
             },
@@ -48,11 +48,6 @@
                         id: 'wikipedia',
                         pattern: '?id <http://ldf.fi/norssit/wikipedia> [] .',
                         label: 'Wikipedia'
-                    },
-                    {
-                        id: 'kansallisbiografia',
-                        pattern: '?id <http://ldf.fi/norssit/kansallisbiografia> [] .',
-                        label: 'Kansallisbiografia'
                     },
                     {
                         id: 'kirjasampo',
@@ -100,66 +95,55 @@
             },
             familyName: {
                 facetId: 'familyName',
-                predicate: '<http://schema.org/familyName>',
+                predicate: '<http://www.w3.org/2004/02/skos/core#prefLabel>',
                 name: 'Sukunimi'
+            },
+            birthYear: {
+                facetId: 'birthYear',
+                predicate: '^<http://www.cidoc-crm.org/cidoc-crm/P98_brought_into_life>/<http://ldf.fi/nbf/time>',
+                name: 'Synnyinvuosi',
+                enabled: true
             },
             birthPlace: {
                 facetId: 'birthPlace',
-                predicate: '<http://ldf.fi/schema/person_registry/birthPlace>',
+                predicate: '^<http://www.cidoc-crm.org/cidoc-crm/P98_brought_into_life>/<http://ldf.fi/nbf/place>',
                 name: 'Syntymäpaikka',
                 enabled: true
             },
-            enrollmentYear: {
-                facetId: 'enrollmentYear',
-                predicate: '<http://ldf.fi/schema/person_registry/enrollmentYear>',
-                name: 'Aloittamisvuosi',
+            deathYear: {
+                facetId: 'birthYear',
+                predicate: '^<http://www.cidoc-crm.org/cidoc-crm/P100_was_death_of>/<http://ldf.fi/nbf/time>',
+                name: 'Kuolinvuosi',
                 enabled: true
             },
-            matriculationYear: {
-                facetId: 'matriculationYear',
-                predicate: '<http://ldf.fi/schema/person_registry/matriculationYear>',
-                name: 'Valmistumisvuosi',
+            deathPlace: {
+                facetId: 'deathPlace',
+                predicate: '^<http://www.cidoc-crm.org/cidoc-crm/P100_was_death_of>/<http://ldf.fi/nbf/place>',
+                name: 'Kuolinpaikka',
                 enabled: true
             },
             occupation: {
                 facetId: 'occupation',
-                predicate: '^<http://ldf.fi/schema/bioc/title_inheres_in>/<http://ldf.fi/schema/bioc/relates_to_title>',
+                predicate: '<http://ldf.fi/schema/bioc/has_profession>',
                 name: 'Arvo tai ammatti',
                 enabled: true
             },
-            education: {
-                facetId: 'education',
-                predicate: '^<http://ldf.fi/schema/bioc/education_inheres_in>/<http://ldf.fi/schema/bioc/relates_to_title>',
-                name: 'Koulutus',
+            period: {
+            	facetId: 'period',
+                predicate: '<http://ldf.fi/nbf/has_period>', 
+                name: 'Aikakausi',
                 enabled: true
             },
-            organization: {
-                facetId: 'organization',
-                predicate: '^<http://ldf.fi/schema/bioc/title_inheres_in>/<http://ldf.fi/schema/bioc/relates_to>',
-                name: 'Työpaikka tai oppilaitos',
+            category: {
+                facetId: 'category',
+                predicate: '<http://ldf.fi/nbf/has_category>', 
+                name: 'Kategoria',
                 enabled: true
             },
             gender: {
                 facetId: 'gender',
                 predicate: '<http://schema.org/gender>',
                 name: 'Sukupuoli',
-                enabled: true
-            },
-            hobby: {
-                facetId: 'hobby',
-                predicate: '<http://schema.org/hobby>',
-                name: 'Harrastus'
-            },
-            medal: {
-                facetId: 'medal',
-                predicate: '^<http://ldf.fi/schema/bioc/title_inheres_in>/<http://ldf.fi/schema/bioc/relates_to_medal>',
-                name: 'Kunniamerkit ja -mitalit',
-                enabled: true
-            },
-            rank: {
-                facetId: 'rank',
-                predicate: '^<http://ldf.fi/schema/bioc/title_inheres_in>/<http://ldf.fi/schema/bioc/relates_to_rank>',
-                name: 'Sotilasarvo tai -toimi',
                 enabled: true
             }
         };
@@ -180,6 +164,10 @@
         ' PREFIX skos: <http://www.w3.org/2004/02/skos/core#> ' +
         ' PREFIX xml: <http://www.w3.org/XML/1998/namespace> ' +
         ' PREFIX bioc: <http://ldf.fi/schema/bioc/> ' +
+        ' PREFIX nbf: <http://ldf.fi/nbf/> ' +
+        ' PREFIX categories:	<http://ldf.fi/nbf/categories/> ' +
+        ' PREFIX occupations: <http://ldf.fi/nbf/occupations/> ' +
+        ' PREFIX crm: <http://www.cidoc-crm.org/cidoc-crm/> ' +
         ' PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> ';
 
         // The query for the results.
@@ -190,48 +178,27 @@
         '    <RESULT_SET> ' +
         '  } ' +
         '  OPTIONAL { ?id schema:givenName ?givenName . }' +
-        '  OPTIONAL { ?id schema:familyName ?familyName . }' +
-        '  OPTIONAL { ?id person_registry:birthPlace ?birthPlace . } ' +
+        '  OPTIONAL { ?id skos:prefLabel ?familyName . }' +
+        '  OPTIONAL { ?id ^crm:P98_brought_into_life/nbf:place ?birthPlace . } ' +
         '  OPTIONAL { ?id norssit:ordinal ?ordinal . } ' +
         '  OPTIONAL { ?id norssit:register_id ?registerNumber . } ' +
-        '  OPTIONAL { ?id schema:birthDate ?birthDate . }' +
-        '  OPTIONAL { ?id schema:deathDate ?deathDate . }' +
+        '  OPTIONAL { ?id ^crm:P98_brought_into_life/nbf:time ?birthDate . }' +
+        '  OPTIONAL { ?id ^crm:P100_was_death_of/nbf:time ?deathDate . }' +
+        '  OPTIONAL { ?id ^crm:P100_was_death_of/nbf:place ?deathPlace . }' +
         '  OPTIONAL { ?id schema:gender ?gender . }' +
         '  OPTIONAL { ?id schema:hobby/skos:prefLabel ?hobby . }' +
         '  OPTIONAL { ?id schema:image ?images . }' +
         '  OPTIONAL { ?id dct:description ?description . }' +
         '  OPTIONAL { ?id person_registry:pageNumber ?pageNumber . }' +
         '  OPTIONAL { ?id person_registry:pageImageURL ?pageImageURL . }' +
-        '  OPTIONAL { ?id person_registry:relatedNorssi ?relatedNorssi . }' +
         '  OPTIONAL { ?id person_registry:enrollmentYear ?enrollmentYear . }' +
         '  OPTIONAL { ?id person_registry:matriculationYear ?matriculationYear . }' +
         '  OPTIONAL { ?id person_registry:entryText ?entryText . }' +
         '  OPTIONAL { ?id norssit:wikipedia ?wikipedia . }' +
         '  OPTIONAL { ?id norssit:viaf ?viaf . }' +
-        '  OPTIONAL { ?id norssit:kansallisbiografia ?kansallisbiografia . }' +
         '  OPTIONAL { ?id norssit:wikidata ?wikidata . }' +
-        '  OPTIONAL { ?id norssit:ulan ?ulan . }' +
-        '  OPTIONAL { ?id norssit:warsa ?warsa . }' +
-        '  OPTIONAL { ?id norssit:kirjasampo ?kirjasampo . }' +
-        '  OPTIONAL { ?id norssit:kulttuurisampo ?kulttuurisampo . }' +
-        '  OPTIONAL { ?id norssit:kb ?kb . }' +
-        '  OPTIONAL { ?id norssit:genitree ?genitree . }' +
-        '  OPTIONAL { ?id norssit:genicom ?genicom . }' +
-        '  OPTIONAL { ?id norssit:sls_biografi ?blf . }' +
-        '  OPTIONAL { ?id ^bioc:title_inheres_in/bioc:relates_to_title/skos:prefLabel ?occupation . }' +
-        '  OPTIONAL { ?id ^bioc:title_inheres_in/bioc:relates_to/skos:prefLabel ?organization . }' +
-        '  OPTIONAL { ' +
-        '  	?id bioc:has_family_relation [' +
-        '    	bioc:inheres_in ?relative__id ;' +
-        '      	a/skos:prefLabel ?relative__type ] .' +
-        '    	FILTER (LANG(?relative__type)="fi")' +
-        '    	?relative__id schema:familyName ?relative__familyName ; schema:givenName ?relative__givenName .' +
-        '		BIND (replace(concat(?relative__givenName," ",?relative__familyName),"[(][^)]+[)]\s*","") AS ?relative__name) ' +
-        '  }' +
-        '  BIND(EXISTS { ' +
-        '   ?ach rdfs:subPropertyOf* nach:involved_in . ' +
-        '   ?id ?ach ?achievement__id . ' +
-        '  } AS ?hasAchievements) ' +
+        '  OPTIONAL { ?id bioc:has_profession/skos:prefLabel ?occupation . }' +
+        '  OPTIONAL { ?id nbf:has_category ?category . }' +
         ' }';
 
         var achievementQuery = prefixes +
@@ -251,8 +218,7 @@
 
         var facetOptions = {
             endpointUrl: endpointConfig.endpointUrl,
-            rdfClass: '<http://xmlns.com/foaf/0.1/Person>',
-            constraint: '?id <http://ldf.fi/norssit/ordinal> ?ordinal . ?id <http://schema.org/familyName> ?familyName .',
+            rdfClass: '<http://ldf.fi/nbf/Person>',
             preferredLang : 'fi'
         };
 
