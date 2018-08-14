3aa621d733e208becdd9aa88fed6a92f96959b62
Petri Leskinen <petri.leskinen@aalto.fi>
2018-08-14 16:54:52 +0300

    Added person preview tooltips, 2

---
+++ src/scripts/common/directives/nbf-person.directive.js
@@ -0,0 +1,28 @@
+(function() {
+    'use strict';
+	angular.module('facetApp')
+	.directive('nbfPerson', function() {
+		return {
+			restrict: 'AE',
+			scope: {
+				url: '@'
+			},
+			controller: ['$scope', 'nbfService', function($scope, nbfService){
+		        
+		        $scope.link = '#!/'+ ($scope.url).replace(new RegExp('/', 'g'), '~2F');
+		        $scope.image = 'images/person_placeholder.svg';
+		        $scope.lifespan = '';
+		        
+		        nbfService.getPopover($scope.url).then(function(data) {
+		        	if (data.length) data = data[0];
+		        	
+		        	$scope.label = data.label;
+		        	if (data.lifespan != '( - )') $scope.lifespan = data.lifespan;
+		        	
+		        	if (data.hasOwnProperty('image')) $scope.image = data.image;
+		        	
+		        });
+			}],
+			template: '<a uib-popover-template="\'views/personTooltipTemplate.html\'" popover-trigger="\'mouseenter\'" ng-href="{{ link }}">{{ label }}</a>'
+		}});
+})();
